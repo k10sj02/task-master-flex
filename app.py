845655909059f1a0cf82ce863125ed045ead51eb
Stann-Omar Jones <35823259+k10sj02@users.noreply.github.com>
2025-12-07 06:21:46 -0500

    deploy: remove __main__ block for Gunicorn compatibility

--- app.py
+++ app.py
@@ -66,8 +66,3 @@ def update(id):
 
     else:
         return render_template('update.html', task=task)
-
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
